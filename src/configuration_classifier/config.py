7cc9fc68cbc066d40f43b770fde70137a0c0e4f1
imamnurby <imamnurby@gmail.com>
2022-10-27 04:03:02 +0000

    add config.py

---
+++ src/configuration_classifier/config.py
@@ -0,0 +1,27 @@
+import os
+from datetime import datetime
+
+base_config = {
+    # model
+    'model_path': 'microsoft/codebert-base',
+    
+    # dataset_path
+    'dataset_path': '../../data_training/config_classifier/train.csv',
+
+    # training config
+    'epochs': 30,
+    'learning_rate': 5e-5,
+    'batch_size': 32,
+    'gradient_accumulation_steps': 1,
+    'num_epochs': 50,
+    'learning_rate': 2e-5,
+    'warmup_steps': 1000,
+
+}
+
+def initialize_config(config):
+    config_cp = config.copy()
+    config_cp['output_dir'] = f'output/{config_cp["model_path"].split("/")[-1]}-'+datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    os.makedirs(config_cp['output_dir'], exist_ok=True)
+
+    return config_cp
