7f71e8a4bc7cf5bcb59298757734d4272e3b8a32
imamnurby <imamnurby@gmail.com>
2022-11-02 02:34:37 +0000

    add predict functionality

--- src_streamlit/backend_utils.py
+++ src_streamlit/backend_utils.py
@@ -45,21 +45,21 @@ def load_retrieval_model_lexical(tokenizer_path, max_k, db_metadata):
     db_metadata (pandas dataframe): a dataframe containing metadata information about the library
     
     Returns:
-    retriever: a retrieval model
+    retrieval_model: a retrieval model
     '''
     # generate index
     index_list = generate_index(db_metadata[['id', 'library']])
 
     # load model
     tokenizer = wrappedTokenizer.from_pretrained(tokenizer_path['lexical'])
-    retriever = retrieve.BM25Okapi(
+    retrieval_model = retrieve.BM25Okapi(
         key='id',
         on='library',
         documents=index_list,
         k=max_k,
         tokenizer=tokenizer
     )
-    return retriever
+    return retrieval_model
 
 
 def load_retrieval_model_deep_learning(model_path, max_k, db_metadata):
@@ -72,22 +72,22 @@ def load_retrieval_model_deep_learning(model_path, max_k, db_metadata):
     db_metadata (pandas dataframe): a dataframe containing metadata information about the library
     
     Returns:
-    retriever: a retrieval model
+    retrieval_model: a retrieval model
     '''
     # generate index
     index_list = generate_index(db_metadata[['id', 'library']])
 
     # load model
-    retriever = retrieve.Encoder(
+    retrieval_model = retrieve.Encoder(
         key='id',
         on='library',
         encoder=SentenceTransformer(model_path).encode,
         k=max_k,
         path=f"../temp/dl.pkl"
     )
-    retriever = dl_retriever.add(documents=index_list)
+    retrieval_model = dl_retriever.add(documents=index_list)
     
-    return retriever
+    return retrieval_model
 
 def load_generative_model_codebert(model_path):
     '''
@@ -98,11 +98,11 @@ def load_generative_model_codebert(model_path):
     
     Returns:
     tokenizer: a huggingface tokenizer
-    model: a generative model to generate API pattern given the library name as the input
+    generative_model: a generative model to generate API pattern given the library name as the input
     '''
     tokenizer = RobertaTokenizer.from_pretrained(model_path)
-    model = EncoderDecoderModel.from_pretrained(model_path)
-    return tokenizer, model
+    generative_model = EncoderDecoderModel.from_pretrained(model_path)
+    return tokenizer, generative_model
 
 
 def get_metadata_library(id_, db_metadata):
@@ -136,43 +136,128 @@ def get_metadata_library(id_, db_metadata):
     
     return metadata_dict
 
-def retrieve_library(model, model_input):
+def id_to_libname(id_, db_metadata):
+    '''
+    Function to convert a library id to its library name
+
+    Params:
+    id_ (int): a unique library id
+    db_metadata (pandas dataframe): a dataframe containing metadata information about the library
+
+    Returns:
+    library_name (string): the library name that corresponds to the input id
+    '''
+    temp_db = db_metadata[db_metadata.id==id_]
+    assert(len(temp_db)==1)
+    library_name = temp_db.iloc[0].library
+    return library_name
+
+
+def retrieve_libraries(retrieval_model, model_input, db_metadata):
     '''
     Function to retrieve a set of relevant libraries using a model based on the input query
 
     Params:
-    model: a model to perform retrieval
+    retrieval_model: a model to perform retrieval
     model_input (string): an input query from the user
 
     Returns:
-    results (list): a list of library unique id
+    library_ids (list): a list of library unique ids
+    library_names (list): a list of library names
     '''
-    results = model(model_input)
-    results = [item.get('id') for item in results]
-    return results
+    results = retrieval_model(model_input)
+    library_ids = [item.get('id') for item in results]
+    library_names = [id_to_libname(item) for item in library_ids]
+    return library_ids, library_names
 
-def generate_api_patterns(model, tokenizer, model_input, num_beams, num_return_sequences):
+def prepare_input_generative_model(library_ids, db_constructor):
+    '''
+    Function to prepare the input of the model to generate API usage patterns
+
+    Params:
+    library_ids (list): a list of library ids
+    db_constructor (pandas dataframe): a dataframe containing the mapping of library names to valid constructor
+
+    Returns:
+    output_dict (dictionary): a dictionary where the key is library id and the value is a list of valid inputs
+    '''
+    output_dict = {}
+    for id_ in library_ids:
+        temp_db = db_constructor[db_constructor.id==id_]
+        output_dict[id_] = []
+        for id__, library_name, methods, constructor:
+            output_dict[id_].append(
+                f'{library_name} [SEP] {constructor}'
+            )
+    return output_dict
+
+def generate_api_usage_patterns(generative_model, tokenizer, model_input, num_beams, num_return_sequences):
     '''
     Function to generate API usage patterns
 
     Params:
-    model: a huggingface model
+    generative_model: a huggingface model
     tokenizer: a huggingface tokenizer
     model_input (string): a string in the form of <library-name> [SEP] constructor
     num_beams (int): the beam width used for decoding
     num_return_sequences (int): how many API usage patterns are returned by the model
 
     Returns:
-    results (list): a list of API usage patterns
+    api_usage_patterns (list): a list of API usage patterns
     '''
     model_input = tokenizer(model_input, return_tensors='pt').input_ids
-    model_output = model.generate(
+    model_output = generative_model.generate(
         model_input,
         num_beams=num_beams,
         num_return_sequences=num_return_sequences
     )
-    results = tokenizer.batch_decode(
+    api_usage_patterns = tokenizer.batch_decode(
         model_output,
         skip_special_tokens=True
     )
-    return results
+    return api_usage_patterns
+
+def predict(
+    input_query, 
+    model_retrieval, 
+    model_generative, 
+    tokenizer_generative, 
+    db_metadata, 
+    db_constructor,
+    num_beams,
+    num_return_sequences
+    ):
+    '''
+    Function to retrieve relevant libraries, generate API usage patterns for each library, and predict the hardware configuration
+
+    Params:
+    input
+    '''
+    library_ids, library_names = retrieve_libraries(model_retrieval, input_query, db_metadata)
+
+    input_generative_model_dict = prepare_input_generative_model(library_ids, db_constructor)
+
+    api_usage_patterns_dict = {}
+    for id_ in input_generative_model_dict:
+        for input_generative_model in input_generative_model_dict.get(id_):
+            api_usage_patterns = generate_api_usage_patterns(
+                model_generative,
+                tokenizer_generative,
+                input_generative_model,
+                num_beams,
+                num_return_sequences
+            )
+
+            temp = input_generative_model.split("[SEP]")
+            library_name = temp[0].strip()
+            constructor = temp[1].strip()
+
+            if library_name not in api_usage_patterns_dict:
+                api_usage_patterns_dict[library_name] = {}
+            
+            api_usage_patterns_dict[library_name][constructor] = api_usage_patterns
+    
+    return api_usage_patterns_dict
+
+
+
