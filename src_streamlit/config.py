8a6e2c141c9c5d84e8d2ceb02d9dcb7c45b42a01
imamnurby <imamnurby@gmail.com>
2022-11-02 18:04:31 +0000

    add more detail on classifier mapping

--- src_streamlit/config.py
+++ src_streamlit/config.py
@@ -23,9 +23,46 @@ config = {
     'max_length': 512
 }
 
+# classifier_class_mapping = {
+#     0: 'UART',
+#     1: 'SPI',
+#     2: 'I2C',
+#     3: 'Explicit declaration'
+# }
+
 classifier_class_mapping = {
-    0: 'UART',
-    1: 'SPI',
-    2: 'I2C',
-    3: 'Explicit declaration'
+    0: {'protocol': 'UART',
+        'pin_mapping_hardware_to_arduino': {
+            'arduino_mega': {
+                'RX-TX': ['0-1', '19-18', '17-16', '15-14'], 
+            },
+            'arduino_uno': {
+                'RX-TX': ['0-1']
+            },
+        }
+    },
+    1: {'protocol': 'SPI',
+        'pin_mapping_hardware_to_arduino': {
+            'arduino_mega': {
+                'SCK-MOSI-MISO-CS': ['52-51-50-53'], 
+            },
+            'arduino_uno': {
+                'SCK-MOSI-MISO-CS': ['13-11-12-10']
+            },
+        },
+    },
+    2: {'protocol': 'I2C',
+        'pin_mapping_hardware_to_arduino': {
+            'arduino_mega': {
+                'SDA-SCL': ['20-21'], 
+            },
+            'arduino_uno': {
+                'SDA-SCL': ['A4-A5']
+            },
+        },
+    },
+    3: {'protocol': 'Explicit declaration',
+        'pin_mapping_hardware_to_arduino': 'any digital or analog pins'
+    }
 }
+
