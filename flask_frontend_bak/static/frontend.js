a687791700b58cd860017e861cf92e4394452115
imamnurby <imamnurby@gmail.com>
2022-11-23 08:05:13 +0000

    before delete accordion inner

--- flask_frontend_bak/static/frontend.js
+++ flask_frontend_bak/static/frontend.js
@@ -161,38 +161,28 @@ const response = [
       // html +=         'Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven\'t heard of them accusamus labore sustainable VHS.'
       // begin usage pattern
       Object.keys(response[i]["usage_patterns"]).forEach(function(key) {        
-        html += '<h6>Sensor: '+ key+ '</h6>';
         let usages_arr = response[i]["usage_patterns"][key]
 
         // begin accordion inner
-        // html += '<div class="accordion" id="accordionExample">'
-        // // begin item
-        // html += ' <div class="accordion-item">'
-        // html += '   <h2 class="accordion-header" id="headingOne">'
-        // html += '     <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">'
-        // html += '       Accordion Item #1'
-        // html += '     </button>'
-        // html += '   </h2>'
-        // html += '   <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">'
-        // html += '     <div class="accordion-body">'
-        // html += '       <strong>This is the first item\'s accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It\'s also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.'
-        // html += '     </div>'
-        // html += '   </div>'
-        // html += ' </div>'
-        // //end item
-        // html += '</div>'
-        // end accordion inner
+        html += '<div class="accordion" id="accordionInner">'
+        // begin item
+        html += ' <div class="accordion-item">'
+        html += '   <h2 class="accordion-header" id="headingOneInner'+ key + i +'">'
+        html += '     <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOneInner'+ key + i +'" aria-expanded="true" aria-controls="collapseOneInner'+ key + i +'">'
+        html += '       Sensor Object: '+ key
+        html += '     </button>'
+        html += '   </h2>'
+        html += '   <div id="collapseOneInner'+ key + i +'" class="accordion-collapse collapse show" aria-labelledby="headingOneInner'+ key + i +'" data-bs-parent="#accordionInner">'
+        html += '     <div class="accordion-body">'
         
-        
-
         // being accordion inner
-        html += '<div style="margin-bottom: 15px" class="accordion" id="accordionPanelsStayOpenExample"' + response[i]["id"] + key + '>';
+        html += '<div style="margin-bottom: 15px" class="accordion" id="accordionPanelsStayOpenExample"' + response[i]["id"] + key + '>'; //1
         
         for (let x=0; x < usages_arr.length; x++){
           let pattern = usages_arr[x].split(" ")
           
-          html += '<div data-bs-toggle="collapse" data-bs-target="#patternCollapse' + response[i]["id"] + key + x + '">';
-          html += '<div class="accordion-item">';
+          html += '<div data-bs-toggle="collapse" data-bs-target="#patternCollapse' + response[i]["id"] + key + x + '">'; //2
+          html += '<div class="accordion-item">'; //3
           html += '<h2 class="accordion-header" id="panelsStayOpen-heading' + response[i]["id"] + key +'">';
 
           if (x == 0){
@@ -205,7 +195,8 @@ const response = [
           html += 'Pattern ' + (x+1);
           html += '</button>';
           html += '</h2>';
-
+          
+          //4
           if (x== 0){
             html += '<div style="padding-top: 10px" id="panelsStayOpen-collapse' + response[i]["id"] + key + x + '" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-heading' + response[i]["id"] + key + x + '">';
           } else {
@@ -218,11 +209,60 @@ const response = [
           }
           html += '</ul>';
           
-          html += '</div>';
-          html += '</div>';
-          html += '</div>';
+          html += '</div>'; //4
+          html += '</div>'; //3
+          html += '</div>'; //2
         }
+        html += '</div>' //1
+
+        html += '     </div>'
+        html += '   </div>'
+        html += ' </div>'
+        //end item
         html += '</div>'
+        // end accordion inner
+        
+        
+
+        // being accordion inner
+        // html += '<div style="margin-bottom: 15px" class="accordion" id="accordionPanelsStayOpenExample"' + response[i]["id"] + key + '>'; //1
+        
+        // for (let x=0; x < usages_arr.length; x++){
+        //   let pattern = usages_arr[x].split(" ")
+          
+        //   html += '<div data-bs-toggle="collapse" data-bs-target="#patternCollapse' + response[i]["id"] + key + x + '">'; //2
+        //   html += '<div class="accordion-item">'; //3
+        //   html += '<h2 class="accordion-header" id="panelsStayOpen-heading' + response[i]["id"] + key +'">';
+
+        //   if (x == 0){
+        //     html += '<button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapse' + response[i]["id"] + key + x + '" aria-expanded="true" aria-controls="panelsStayOpen-collapse' + response[i]["id"] + key + x + '">';
+        //   }
+        //   else {
+        //     html += '<button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapse' + response[i]["id"] + key + x + '" aria-expanded="false" aria-controls="panelsStayOpen-collapse' + response[i]["id"] + key + x + '">';
+        //   }
+
+        //   html += 'Pattern ' + (x+1);
+        //   html += '</button>';
+        //   html += '</h2>';
+          
+        //   //4
+        //   if (x== 0){
+        //     html += '<div style="padding-top: 10px" id="panelsStayOpen-collapse' + response[i]["id"] + key + x + '" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-heading' + response[i]["id"] + key + x + '">';
+        //   } else {
+        //     html += '<div style="padding-top: 10px" id="panelsStayOpen-collapse' + response[i]["id"] + key + x + '" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-heading' + response[i]["id"] + key + x + '">';
+        //   }
+          
+        //   html += '<ul>';
+        //   for (let y=0; y<pattern.length; y++){
+        //     html += '<li>' + pattern[y] + '</li>';
+        //   }
+        //   html += '</ul>';
+          
+        //   html += '</div>'; //4
+        //   html += '</div>'; //3
+        //   html += '</div>'; //2
+        // }
+        // html += '</div>' //1
       });
       // end usage pattern 
       
@@ -234,78 +274,13 @@ const response = [
       html += '</div>';
       // end accordion outer
       
-      
-      // // interface configuration begin
-      // html += '<h5 class="card-title text-primary">Interface Configuration</h5>';
-      // html += '<h6>Protocol: '+ response[i]["hw_config"]["protocol"]+ '</h6>';
-      // html += '<table class="table table-hover"><thead><tr><th>Arduino Type</th><th>I/O hardware --> Arduino</th></tr></thead><tbody>'
-      // //loop through hardware config
-      // for (const property in response[i]["hw_config"]["pin_connection_from_hw_to_arduino"]){
-      //   let firstLetter = property.charAt(0).toUpperCase();
-      //   let remLetter = property.substring(1).split("_");
-      //   html += '<tr><td>' + firstLetter + remLetter[0] + '_' + remLetter[1].charAt(0).toUpperCase() + remLetter[1].substring(1) + '</td><td>';
-      //   //loop through the arduino type
-      //   for (let y=0; y < response[i]["hw_config"]["pin_connection_from_hw_to_arduino"][property].length; y++){
-      //     html += '(' + (response[i]["hw_config"]["pin_connection_from_hw_to_arduino"][property][y][0] + ',' + response[i]["hw_config"]["pin_connection_from_hw_to_arduino"][property][y][1]) + ') ';        
-      //   }
-      //   html += ' </td></tr>';
-      // }
-      // html += '</tbody></table> ';
-      // end interface configuration
-      // begin usage-pattern
-      // html +=       '<h5 style="margin-top: 10px" class="card-title text-primary">Usage Patterns</h5>'
       //button close
       html +=       '<div class="position-absolute top-0 end-0">'
       html +=         '<button type="button" class="btn-close" aria-label="Close" onclick="onOut('+ response[i]["id"] +')"></button>';
       html +=       '</div>';
       // end button close
       
-      // begin usage pattern
-      // Object.keys(response[i]["usage_patterns"]).forEach(function(key) {        
-      //   html += '<h6>Sensor: '+ key+ '</h6>';
-      //   html += '<div style="margin-bottom: 15px" class="accordion" id="accordionPanelsStayOpenExample"' + response[i]["id"] + key + '>';
-      //   let usages_arr = response[i]["usage_patterns"][key]
-
-      //   for (let x=0; x < usages_arr.length; x++){
-      //     let pattern = usages_arr[x].split(" ")
-          
-      //     html += '<div data-bs-toggle="collapse" data-bs-target="#patternCollapse' + response[i]["id"] + key + x + '">';
-      //     html += '<div class="accordion-item">';
-      //     html += '<h2 class="accordion-header" id="panelsStayOpen-heading' + response[i]["id"] + key +'">';
-
-      //     if (x == 0){
-      //       html += '<button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapse' + response[i]["id"] + key + x + '" aria-expanded="true" aria-controls="panelsStayOpen-collapse' + response[i]["id"] + key + x + '">';
-      //     }
-      //     else {
-      //       html += '<button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapse' + response[i]["id"] + key + x + '" aria-expanded="false" aria-controls="panelsStayOpen-collapse' + response[i]["id"] + key + x + '">';
-      //     }
-
-      //     html += 'Pattern ' + (x+1);
-      //     html += '</button>';
-      //     html += '</h2>';
-
-      //     if (x== 0){
-      //       html += '<div style="padding-top: 10px" id="panelsStayOpen-collapse' + response[i]["id"] + key + x + '" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-heading' + response[i]["id"] + key + x + '">';
-      //     } else {
-      //       html += '<div style="padding-top: 10px" id="panelsStayOpen-collapse' + response[i]["id"] + key + x + '" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-heading' + response[i]["id"] + key + x + '">';
-      //     }
-          
-      //     html += '<ul>';
-      //     for (let y=0; y<pattern.length; y++){
-      //       html += '<li>' + pattern[y] + '</li>';
-      //     }
-      //     html += '</ul>';
-          
-      //     html += '</div>';
-      //     html += '</div>';
-      //     html += '</div>';
-      //   }
-      //   html += '</div>'
-      // });
-      // end usage pattern 
-
-      // html += '<h6 class="card-title text-primary">Protocol: </h6><h6>'+ response[i]["hw_config"]["protocol"]+ '</h6></div></div>';
-      html += '</div></div>';
+           html += '</div></div>';
       html += '</div></div>';
       }
   
